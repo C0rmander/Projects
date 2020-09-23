1e37acf077bdb6dd6da0e3c2d3cac11cdeac2302
Cormac Conway <71558726+C0rmander@users.noreply.github.com>
2020-09-23 10:56:03 +0100

    Add files via upload

---
+++ motion_Tracker/serialRotation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class serialRotation : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("\\\\.\\COM14", 115200); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+	float[] lastRot = {0,0,0}; //Need the last rotation to tell how far to spin the camera
+
+
+	void Start () {
+		stream.Open(); //Open the Serial Stream.
+	}
+
+	// Update is called once per frame
+	void Update () {
+		string value = stream.ReadLine();
+		 //Read the information
+		string[] vec3 = value.Split(',');
+		Debug.Log (vec3[8].ToString() + "," + value);
+			transform.Rotate(           //Rotate the camera based on the new values
+				float.Parse(vec3[3]),
+				float.Parse(vec3[4]),
+				float.Parse(vec3[5]),
+				Space.Self
+			);
+		;
+			lastRot[0] = float.Parse(vec3[3]);  //Set new values to last time values for the next loop
+			lastRot[1] = float.Parse(vec3[5]);
+			lastRot[2] = float.Parse(vec3[4]);
+			stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+
+	}
+
+	void OnGUI()
+	{
+		string newString = "Connected: " + transform.rotation.x + ", " + transform.rotation.y + ", " + transform.rotation.z;
+		GUI.Label(new Rect(10,10,300,100), newString); //Display new values
+		// Though, it seems that it outputs the value in percentage O-o I don't know why.
+	}
+
+}
---
+++ motion_Tracker/sketch_aug13d.ino
@@ -0,0 +1,81 @@
+
+#include "MPU9250.h"
+
+// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
+MPU9250 IMU(Wire,0x68);
+int status;
+
+int invertion(float x)
+{
+  if (x < 0)
+  {
+    Serial.print(fabsf(x),2);
+    Serial.print(","); 
+  }
+  else
+  {
+  if (x > 0)
+  {
+    x = x-(x*2);
+    Serial.print (x,2);
+    Serial.print(",");
+  }
+  }
+  if(x = 0)
+  {
+    Serial.print(x,2);
+  }
+}
+
+
+void setup() {
+  
+  // serial to display data
+  Serial.begin(115200);
+  while(!Serial) {}
+IMU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
+  // setting the gyroscope full scale range to +/-500 deg/s
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+  // setting DLPF bandwidth to 20 Hz
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_5HZ);
+  // setting SRD to 19 for a 50 Hz update rate
+  IMU.setSrd(19);
+  IMU.calibrateGyro();
+  // start communication with IMU 
+  status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
+}
+
+void loop() {
+  // read the sensor
+  IMU.readSensor();
+  // display the data
+  Serial.print(IMU.getAccelX_mss()*10,1);
+  Serial.print(",");
+  Serial.print(IMU.getAccelY_mss()*10,1);
+  Serial.print(",");
+  Serial.print(IMU.getAccelZ_mss()*10-((IMU.getAccelZ_mss()*10)*2),1);
+  Serial.print(",");
+  invertion(IMU.getGyroX_rads()*10);
+  Serial.print(IMU.getGyroY_rads()*10,2);
+  Serial.print(",");
+  invertion(IMU.getGyroZ_rads()*10);
+  Serial.print(IMU.getMagX_uT()*10,2);
+  Serial.print(",");
+  Serial.print(IMU.getMagY_uT()*10,2);
+  Serial.print(",");
+  Serial.print(IMU.getMagZ_uT()*10,2);
+  Serial.print(",");
+  Serial.println(IMU.getTemperature_C()*10,2);
+  delay(100);
+}
+
+
+
+
